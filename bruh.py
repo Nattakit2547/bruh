ae654d13da18b521998262064fad9c290dfd0bbf
Mr.Guide ren zao <nattakid3320@gmail.com>
2024-06-26 11:39:31 +0700

    b

--- bruh.py
+++ bruh.py
@@ -1,4 +1,16 @@
 a = 3.75
 b = "renzao by Mr.guide"
+c = 1
+d = 2.79j
+f = a+c**d
+
+if (a == 3.75)or(d == 2.79j):
+    print("Mother")
+
 print("Hello,World")
 print(b)
+print(a,b)
+print(c, type(c))
+print(a, type(a))
+print(d, type(d))
+print(f, type(f))
